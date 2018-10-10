5761a8603003d6bae025bb110ece40390dd210e2
Kaka.xie <kaka.xie@mrobot.cn>
2018-10-10 16:06:15 +0800

    add function FPIGetInfo to get lib info

--- scripts/main.py
+++ scripts/main.py
@@ -16,9 +16,10 @@ def main():
     #dev_so = ctypes.CDLL('libFPDev_WL.so')
     dev_so = ctypes.CDLL('libFPDevUSB_WL.so')
     #alg_so = ctypes.CDLL('libFPAlg_WL.so')
-    alg_so = ctypes.CDLL('libFPDevUSB_WL.so')
+    #alg_so = ctypes.CDLL('libFPDevUSB_WL.so')
 
     dev_info = (ctypes.c_char * (64 + 1))()
+    lib_info = (ctypes.c_char * (64 + 1))()
     err_msgs = (ctypes.c_char * (64 + 1))()
     sMB = (ctypes.c_char * (512 + 1))()
     sTZ = (ctypes.c_char * (512 + 1))()
@@ -28,15 +29,28 @@ def main():
     term = -1
     port = 0
     ret = -1 
+
     os.system("clear")
+
     print "start to get dev version ... "
     #ret = dev_so.FPIGetVersion(term, port, 3000, dev_info, err_msgs)
     ret = dev_so.FPIGetVersion(dev_info, err_msgs)
     print ' ---------- end of getting dev version --------------'
+
     if ret < 0:
         rospy.logerr("ERROR: FPIGetVersion error ! !")
     else:
         rospy.loginfo("dev_info:  %s", dev_info.value)
+
+    print "start to get lib info ... "
+    ret = dev_so.FPIGetInfo(lib_info, err_msgs)
+    print ' ---------- end of getting lib info --------------'
+
+    if ret < 0:
+        rospy.logerr("ERROR: FPIGetInfo error ! !")
+    else:
+        rospy.loginfo("dev_info:  %s", lib_info.value)
+
 
     #sys.system('clear')
     raw_input("press enter to continue . . .")
@@ -46,8 +60,7 @@ def main():
                 "\t\ttest function      \n" \
                 "\t\t===================================================\n" \
                 "\t\t\t1-- get template (press 3 times) and save template \n"\
-                "\t\t\t2--pick fingerprint feature  \n" \
-                "\t\t\t3--fingerprint match\n" \
+                "\t\t\t2--pick fingerprint feature and match \n" \
                 "\t\t\t4--finger check(check the finger is pressed or not)\n" \
                 "\t\t\t0--quit        \n"
 
@@ -75,9 +88,7 @@ def main():
             elif ret == 0:
                 rospy.loginfo("FPIGetTemplate excute OK")
                 rospy.loginfo("\n采集指纹模板成功\n")
-                rospy.logwarn("\n ret:[%d], err_msgs:[%s]", ret, err_msgs.value)
                 rospy.loginfo("data len:  %d", data_len.value)
-                #for i in range(0, len(sMB)):
                 rospy.loginfo("template info:  %s", sMB.value)
 
             print 'ret value: ', ret
@@ -86,7 +97,6 @@ def main():
 
             print "start to pick fingerprint feature ..."
             data_len.value = 0
-            #sTZ_tmp = (ctypes.c_char * (512 + 1))()
             #ret = dev_so.FPIGetFeature(term, port, 5000, sTZ, ctypes.byref(data_len), err_msgs)
             ret = dev_so.FPIGetFeature(5000, sTZ, ctypes.byref(data_len), err_msgs)
             print ' ---------- pick fingerprint feature --------------'
@@ -99,19 +109,6 @@ def main():
                 rospy.loginfo("data len:  %d", data_len.value)
                 rospy.loginfo("feature info: %s", sTZ.value)
 
-#            ret = alg_so.FPIGetFeatureByImg(sTZ_tmp, sTZ , ctypes.byref(data_len), err_msgs)
-#
-#            if ret < 0:
-#                rospy.logerr("ERROR: FPIGetFeatureByImg error ! !")
-#                rospy.logerr("\nFPIGetFeatureByImg--[%d] [%s]", ret, err_msgs.value)
-#            else:
-#                rospy.loginfo("FPIGetFeatureByImg excute OK")
-#                rospy.loginfo("data len:  %d", data_len.value)
-#                rospy.loginfo("feature info: %s", sTZ.value)
-
-
-        elif state == 3:
-            
             print "\n           start to match ..."
             ret = dev_so.FPIFpMatch(sMB, sTZ, 3)
             print ' ---------- end match --------------\n'
@@ -122,6 +119,9 @@ def main():
             else:
                 rospy.loginfo("FPIFpMatch excute OK")
                 rospy.loginfo("\n指纹比对成功")
+
+        elif state == 3:
+            pass
 
         elif state == 4:
 
